2ad1237fb35147605e430e72b657edbf11bd26e7
MANOJHRMANOJHR <manojhr799@gmail.com>
2025-09-19 00:20:08 +0530

    dbchangedfromlocalhosttoatlas

--- api/config/db.js
+++ api/config/db.js
@@ -1,6 +1,43 @@
-const mongoose = require('mongoose');
+//const mongoose = require('mongoose');
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
 // false: passes the filter query to mongodb even if it is not in the schema and returns empty document
 //true: removes the filter query not defined in the schema and returns the all the document
+
+const connectWithDB = () => {
+
+
+
+const uri = process.env.DB_URL;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+}
+
+
+/*
 const connectWithDB = () => {
   mongoose.set('strictQuery', false);
   mongoose
@@ -17,5 +54,5 @@ const connectWithDB = () => {
       
     });
 };
-
+*/
 module.exports = connectWithDB;
